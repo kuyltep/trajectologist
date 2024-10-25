27ac5071f4cce233302ef08ad0c33597a8b211d8
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-10-26 01:34:50 +0300

    add profession more info

---
+++ my-app/components/screens/ProfessionMoreInfoScreen.tsx
@@ -0,0 +1,195 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@/store/store";
+import { useGetProfessionAllInfoQuery } from "@/api/selectedApiSlice";
+import { usePostSelectUserProfessionMutation } from "@/api/selectedApiSlice";
+import { IProfessionAllInfo } from "@/api/types/IProfession";
+import { useGetProfileQuery } from "@/api/loginApiSlice";
+import { setMoreInfoProfession } from "@/store/slices/selectProfession";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
+
+const ProfessionMoreInfoScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const professionId = useSelector(
+    (state: RootState) => state.selected.moreInfoProfessionId
+  );
+  const navigationHook = useNavigation(); // Хук для навигации
+
+  const handleGoBack = () => {
+    navigationHook.goBack(); // Возвращаемся на предыдущий экран
+  };
+  const { data: professionData, isLoading: isProfessionLoading } =
+    useGetProfessionAllInfoQuery({ id: professionId });
+  const { data: userData, isLoading: isUserLoading } = useGetProfileQuery(null);
+
+  const [userCompetenciesInProfession, setUserCompetenciesInProfession] =
+    useState([]);
+  const [competenciesToAcquire, setCompetenciesToAcquire] = useState([]);
+  useEffect(() => {
+    dispatch(setMoreInfoProfession(professionData));
+
+    if (!isUserLoading && !isProfessionLoading) {
+      const userCompetenciesInProfession = userData?.user_competencies.filter(
+        (userCompetency) =>
+          professionData?.competencies.some(
+            (professionCompetency) =>
+              professionCompetency.id === userCompetency.competency.id
+          )
+      );
+      const competenciesToAcquire = professionData.competencies.filter(
+        (professionCompetency) =>
+          !userData?.user_competencies.some(
+            (userCompetency) =>
+              userCompetency.competency.id === professionCompetency.id
+          )
+      );
+      setCompetenciesToAcquire(competenciesToAcquire);
+      setUserCompetenciesInProfession(userCompetenciesInProfession);
+    }
+  }, [professionData, userData, isUserLoading, isProfessionLoading]);
+
+  const [
+    selectProfession,
+    { isLoading: isSelecting, isSuccess: isSelectSuccess },
+  ] = usePostSelectUserProfessionMutation();
+
+  const handleSelectProfession = async () => {
+    try {
+      if (professionId) {
+        const result = await selectProfession({
+          profession_id: professionId,
+        }).unwrap();
+        console.log(result);
+        navigation("Home");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 60,
+      padding: 20,
+    },
+    backButton: {
+      position: "absolute",
+      top: 20,
+      left: 20,
+      zIndex: 1, //  Чтобы кнопка была поверх фона
+      padding: 5,
+      borderRadius: 5,
+      backgroundColor: "#e57454", // Полупрозрачный черный фон
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      color: "white",
+      marginBottom: 15,
+    },
+    description: {
+      fontSize: 16,
+      color: "white",
+      marginBottom: 20,
+    },
+    infoBlock: {
+      backgroundColor: "#e57454",
+      padding: 15,
+      borderRadius: 15,
+      marginBottom: 20,
+    },
+    infoBlockTitle: {
+      fontSize: 18,
+      fontWeight: "bold",
+      color: "white",
+      marginBottom: 10,
+    },
+    competencyItem: {
+      fontSize: 16,
+      color: "white",
+      marginBottom: 5,
+    },
+    button: {
+      backgroundColor: "#e57454",
+      padding: 15,
+      borderRadius: 15,
+      alignItems: "center",
+    },
+    buttonText: {
+      color: "white",
+      fontSize: 18,
+      fontWeight: "bold",
+    },
+  });
+
+  return professionData && userData ? (
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        source={require("@/assets/images/screen2.jpg")}
+        resizeMode="repeat"
+        resizeMethod="resize"
+      />
+
+      <TouchableOpacity style={styles.backButton} onPress={handleGoBack}>
+        <Icon name="arrow-back" size={24} color="white" />{" "}
+      </TouchableOpacity>
+      <ScrollView style={styles.container}>
+        <View>
+          <Text style={styles.title}>{professionData.name}</Text>
+          <Text style={styles.description}>{professionData.description}</Text>
+
+          <View style={styles.infoBlock}>
+            <Text style={styles.infoBlockTitle}>
+              З/п- от {professionData.salary}
+            </Text>
+          </View>
+
+          <View style={styles.infoBlock}>
+            <Text style={styles.infoBlockTitle}>Ваши навыки:</Text>
+            {userCompetenciesInProfession.length &&
+              userCompetenciesInProfession.map((competency) => (
+                <Text key={competency.id} style={styles.competencyItem}>
+                  - {competency.competency.name}
+                </Text>
+              ))}
+          </View>
+
+          <View style={styles.infoBlock}>
+            <Text style={styles.infoBlockTitle}>
+              Навыки которые стоит приобрести :
+            </Text>
+            {competenciesToAcquire.length &&
+              competenciesToAcquire.map((competency) => (
+                <Text key={competency.id} style={styles.competencyItem}>
+                  - {competency.name}
+                </Text>
+              ))}
+          </View>
+          <View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={handleSelectProfession}
+              disabled={isSelecting}
+            >
+              <Text style={styles.buttonText}>
+                {isSelecting ? "Выбор..." : "Выбрать профессию"}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  ) : null;
+};
+
+export default ProfessionMoreInfoScreen;
