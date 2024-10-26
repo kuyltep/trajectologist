9f4965bf359fe88e982a6bb715830fba8a8e6e77
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-10-27 01:47:36 +0300

    upd store

--- my-app/store/store.ts
+++ my-app/store/store.ts
@@ -1,20 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from "@/store/slices/loginSlice";
-import selectSlice from "@/store/slices/selectProfession";
+import selectSlice from "@/store/slices/selectSlice";
+import userSlice from "@/store/slices/userSile";
 import { loginApiSlice } from "@/api/loginApiSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { selectApiSlice } from "@/api/selectedApiSlice";
+import { userDataApiSlice } from "@/api/userDataApiSlice";
 export const store = configureStore({
   reducer: {
     login: loginSlice,
+    user: userSlice,
     selected: selectSlice,
     [loginApiSlice.reducerPath]: loginApiSlice.reducer,
+    [userDataApiSlice.reducerPath]: userDataApiSlice.reducer,
     [selectApiSlice.reducerPath]: selectApiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(loginApiSlice.middleware)
-      .concat(selectApiSlice.middleware),
+      .concat(selectApiSlice.middleware)
+      .concat(userDataApiSlice.middleware),
 });
 
 setupListeners(store.dispatch);
