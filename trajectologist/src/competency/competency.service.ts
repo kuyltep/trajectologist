cd1b0519f4af9cde4511a71121c3e84b7c40efb9
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-10-27 01:46:34 +0300

    udpate comp service

--- trajectologist/src/competency/competency.service.ts
+++ trajectologist/src/competency/competency.service.ts
@@ -1,16 +1,37 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { Competency, UserCompetency } from '@prisma/client';
+import { Competency, UserCompetency, UserStep } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateUserCompetencyDto } from './dto/update.comptency.dto';
 
 @Injectable()
 export class CompetencyService {
   constructor(private readonly prismaService: PrismaService) {}
-  async getCompetenciesForProfession(id: string) {
+  async getCompetenciesForProfession(id: string, professionId: string) {
     try {
       return await this.prismaService.userCompetency.findMany({
         where: {
+          user: {
+            profession_id: professionId,
+          },
           user_id: id,
+        },
+      });
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  async getCompetencyById(id: string) {
+    try {
+      return await this.prismaService.userCompetency.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          steps: {
+            include: { step: true },
+          },
+          competency: true,
         },
       });
     } catch (error) {
